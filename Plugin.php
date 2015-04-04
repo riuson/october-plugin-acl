da71a78ac79df738b086cc03dfd96809df44c057
Vladimir <riuson@gmail.com>
2015-04-04 21:38:24 +0500

    Added controllers for groups and permissions

--- Plugin.php
+++ Plugin.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Riuson\ACL;
 
 use System\Classes\PluginBase;
@@ -28,10 +27,31 @@ class Plugin extends PluginBase
 
     public function boot()
     {
+        // extend Rainlab.User model
         UserModel::extend(function ($model) {
             $model->hasMany['groups'] = [
                 'riuson\ACL\Models\UserGroup'
             ];
+        });
+
+        // extend Rainlab.User settings
+        \Event::listen('backend.menu.extendItems', function ($manager) {
+            $manager->addSideMenuItems('RainLab.User', 'user', [
+                'groups' => [
+                    'label' => 'Groups',
+                    'icon' => 'icon-users',
+                    'code' => 'groups',
+                    'owner' => 'RainLab.User',
+                    'url' => \Backend::url('riuson/acl/groups')
+                ],
+                'permissions' => [
+                    'label' => 'Permissions',
+                    'icon' => 'icon-key',
+                    'code' => 'permissions',
+                    'owner' => 'RainLab.User',
+                    'url' => \Backend::url('riuson/acl/permissions')
+                ]
+            ]);
         });
     }
 }
---
+++ controllers/Permissions.php
@@ -0,0 +1,25 @@
+<?php namespace Riuson\ACL\Controllers;
+
+use BackendMenu;
+use Backend\Classes\Controller;
+
+/**
+ * Permissions Back-end Controller
+ */
+class Permissions extends Controller
+{
+    public $implement = [
+        'Backend.Behaviors.FormController',
+        'Backend.Behaviors.ListController'
+    ];
+
+    public $formConfig = 'config_form.yaml';
+    public $listConfig = 'config_list.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        BackendMenu::setContext('RainLab.User', 'user', 'permissions');
+    }
+}
